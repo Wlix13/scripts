99d4ebf73f12b60f4196307751320444ea5db43e
Renat <renat@mustafin.su>
2022-11-29 08:45:24 +0300

    Fixes

--- starship.sh
+++ starship.sh
@@ -7,7 +7,7 @@ sudo usermod -s /usr/bin/zsh $(whoami)
 echo $SHELL
 
 # Install Oh-My-ZSH
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
 # Install Starship
 curl -sS https://starship.rs/install.sh | sh
