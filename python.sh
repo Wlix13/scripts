3a6f1d90a89d1239f43e811c0e34fd0292626e45
wlix <renat@mustafin.su>
2023-01-10 18:08:50 +0300

    python

--- python.sh
+++ python.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-export SRC_URL="https://www.python.org/ftp/python/3.10.7/Python-3.10.7.tgz"
+export SRC_URL="https://www.python.org/ftp/python/3.10.9/Python-3.10.9.tgz"
 
 # Install dependencies 
 sudo apt update
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev libbluetooth-dev
 
 
 # Download Python
@@ -12,7 +12,7 @@ wget -q -O- $SRC_URL | tar -xz
 
 # Configure installation
 cd Python-3.* && ./configure --enable-optimizations
-if [ nproc -ge 2 ]; then
+if [ $(nproc) -ge 2 ]; then
     make -j $(nproc)
 else
     make
