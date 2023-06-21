9a9321d273b32bbb1c3552a0de9ae5b41c2aab32
wlix <renat@mustafin.su>
2023-06-21 22:55:40 +0300

    python version update

--- python.sh
+++ python.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export SRC_URL="https://www.python.org/ftp/python/3.11.2/Python-3.11.2.tgz"
+export SRC_URL="https://www.python.org/ftp/python/3.11.4/Python-3.11.4.tgz"
 
 # Install dependencies
 sudo apt update
