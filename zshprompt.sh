6eb81defd844c2e077daa9c6f89ac9f20951687c
wlix <renat@mustafin.su>
2023-06-23 01:43:25 +0300

    Auto FiraCode font reworked

--- zshprompt.sh
+++ zshprompt.sh
@@ -6,7 +6,6 @@ sudo apt upgrade -y
 sudo apt install zsh wget curl unzip git locales fontconfig -y
 
 sudo usermod -s /usr/bin/zsh $(whoami)
-echo $SHELL
 
 # Install Oh-My-ZSH
 curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
@@ -16,10 +15,18 @@ zsh && exit
 curl -fsSL https://starship.rs/install.sh | sudo sh
 sed -i "s/ZSH_THEME=/#ZSH_THEME=/" ~/.zshrc
 
+# Add to .zshrc
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+
 # Add support for FiraCode Nerd Font
-export SRC_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip"
-wget -q $SRC_URL -O ~/FiraCode.zip
-unzip ~/FiraCode.zip -d ~/.local/share/fonts && rm -rf ~/FiraCode.zip
+fonts_dir="~/.local/share/fonts"
+if [ ! -d "${fonts_dir}" ]; then
+    mkdir -p "${fonts_dir}"
+fi
+version=6.2
+zip=Fira_Code_v${version}.zip
+curl --fail --location --show-error https://github.com/tonsky/FiraCode/releases/download/${version}/${zip} --output ${zip}
+unzip -o -q -d ${fonts_dir} ${zip} && rm ${zip}
 fc-cache -fv
 
 # Install Nerd Font Symbols Preset
@@ -29,9 +36,6 @@ wget https://starship.rs/presets/toml/nerd-font-symbols.toml -O ~/.config/starsh
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 sed -i "s/plugins=(git)/plugins=(\ngit\nzsh-autosuggestions\n)/" ~/.zshrc
-
-# Add to .zshrc
-echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 
 # Configure locales
 sudo dpkg-reconfigure locales
