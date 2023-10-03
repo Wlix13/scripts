10a404249070515334ea567766395c9cd47b408c
wlix13 <renat@mustafin.su>
2023-10-03 13:45:08 +0300

    Raspi WiFi configs

--- raspi.sh
+++ raspi.sh
@@ -20,51 +20,15 @@ read -p "Wi-Fi SSID: " SSID
 read -p "Wi-Fi Passphrase: " PSK
 
 # Create a wpa_supplicant.conf file
-echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev" | tee -a /etc/wpa_supplicant.conf
-echo "update_config=1" | tee -a /etc/wpa_supplicant.conf
-echo "country=RU" | tee -a /etc/wpa_supplicant.conf
-echo "network={" | tee -a /etc/wpa_supplicant.conf
-echo "ssid=\"$SSID\"" | tee -a /etc/wpa_supplicant.conf
-echo "psk=\"$PSK\"" | tee -a /etc/wpa_supplicant.conf
-echo "key_mgmt=WPA-PSK" | tee -a /etc/wpa_supplicant.conf
-echo "scan_ssid=1" | tee -a /etc/wpa_supplicant.conf
-echo "}" | tee -a /etc/wpa_supplicant.conf
+cp .config/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant.conf
+sed -i "0,/ssid/s/ssid=/ssid=$SSID/" /etc/wpa_supplicant.conf
+sed -i "0,/psk/s/psk=/psk=$PSK/" /etc/wpa_supplicant.conf
 
 # Auto-connect to Wi-Fi at boot(make wpa_supplicant.service file)
-echo "[Unit]" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "Description=WPA supplicant daemon" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "Before=network.target" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "After=dbus.service" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "Wants=network.target" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "IgnoreOnIsolate=true" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "[Service]" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "Type=dbus" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "BusName=fi.w1.wpa_supplicant1" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "ExecStart=/sbin/wpa_supplicant -u -s -c /etc/wpa_supplicant.conf -i wlan0" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "Restart=always" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "ExecReload=/bin/kill -HUP $MAINPID" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "Group=netdev" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "RuntimeDirectory=wpa_supplicant" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "RuntimeDirectoryMode=0750" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "[Install]" | tee -a /etc/systemd/system/wpa_supplicant.service
-echo "WantedBy=multi-user.target" | tee -a /etc/systemd/system/wpa_supplicant.service
+cp .config/wpa_supplicant/wpa_supplicant.service /etc/systemd/system/wpa_supplicant.service
 
 # Auto-obtain IP address(make dhclient.service file)
-echo "[Unit]" | tee -a /etc/systemd/system/dhclient.service
-echo "Description=DHCP Client" | tee -a /etc/systemd/system/dhclient.service
-echo "Before=network.target" | tee -a /etc/systemd/system/dhclient.service
-echo "After=wpa_supplicant.service" | tee -a /etc/systemd/system/dhclient.service
-echo "" | tee -a /etc/systemd/system/dhclient.service
-echo "[Service]" | tee -a /etc/systemd/system/dhclient.service
-echo "Type=forking" | tee -a /etc/systemd/system/dhclient.service
-echo "ExecStart=/sbin/dhclient wlan0 -v" | tee -a /etc/systemd/system/dhclient.service
-echo "ExecStop=/sbin/dhclient wlp4s0 -r" | tee -a /etc/systemd/system/dhclient.service
-echo "Restart=always" | tee -a /etc/systemd/system/dhclient.service
-echo "" | tee -a /etc/systemd/system/dhclient.service
-echo "[Install]" | tee -a /etc/systemd/system/dhclient.service
-echo "WantedBy=multi-user.target" | tee -a /etc/systemd/system/dhclient.service
+cp .config/wpa_supplicant/dhclient.service /etc/systemd/system/dhclient.service
 
 # Enable services
 systemctl daemon-reload
